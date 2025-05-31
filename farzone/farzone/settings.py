20a4783d61f8e66e9f02d07c5c2a4f201d26d72e
dmitr <dmitriyeltanskiy48@yandex.ru>
2025-06-01 00:05:37 +0300

    готовлюсь к загрузке на сервер

--- farzone/farzone/settings.py
+++ farzone/farzone/settings.py
@@ -83,7 +83,7 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
